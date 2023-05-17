fc3e318108918fec69e414c0e035920337eda8bb
Gurkan <hello@gurkz.me>
2023-05-17 17:20:29 +0200

    setup nextjs

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import './globals.css'
+import { Space_Grotesk } from 'next/font/google'
+
+const grotesk = Space_Grotesk({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'cube bot',
+  description: 'The cube bot',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${grotesk.className} flex flex-col h-screen justify-between`}>
+        {/* main content */}
+        <section className="mb-auto">
+          {children}
+        </section>
+        {/* footer */}
+        <footer>
+
+        </footer>
+      </body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-5xl font-bold">welcome to the cube website</h1>
+    </main>
+  )
+}
