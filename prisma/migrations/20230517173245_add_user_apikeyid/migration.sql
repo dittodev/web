000238c7376cd3f17d8cd565f3cca620cd2b9fbd
Gurkan <hello@gurkz.me>
2023-05-22 15:38:20 +0200

    update to postgresql and change the dashboard

--- prisma/migrations/20230517173245_add_user_apikeyid/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `User` ADD COLUMN `apiKeyID` VARCHAR(191) NULL;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { notFound } from "next/navigation";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "ditto | dashboard",
@@ -12,7 +13,18 @@ const page = async () => {
   const user = await getServerSession(authOptions);
   if (!user) return notFound();
 
-  return <div className="page">dashy dash</div>;
+  return (
+    <div className="page">
+      ditto dashboard (coming NOT soon)
+      <br />
+      <Link
+        href="/dashboard/api"
+        className="underline decoration-themeColor decoration-2 underline-offset-4"
+      >
+        do you want to generate an api key?
+      </Link>
+    </div>
+  );
 };
 
 export default page;
